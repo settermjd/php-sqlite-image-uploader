fd59810aacae0f3b79540450beff1edbffd65146
Matthew Setter <matthew@matthewsetter.com>
2023-07-04 09:10:22 +0200

    Add the database migration

---
+++ data/migrations/Version20230628132906.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Skar\LaminasDoctrineORM;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230628132906 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE image (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(200) NOT NULL, data BLOB NOT NULL)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_C53D045F5E237E06 ON image (name)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_C53D045FADF3F363 ON image (data)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE image');
+    }
+}
