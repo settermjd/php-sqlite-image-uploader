bccd15e9b713b37427d12d4a553c4803a81ff2c2
Matthew Setter <matthew@matthewsetter.com>
2023-07-05 11:31:18 +0200

    Auto-generate the list of supported image formats
    After looking further into the list of available functions, I found
    queryFormats(), which returns a list of the supported formats. What's
    more, the user may not have support for each of the desired image
    formats in their Imagick installation.
    
    Given these two things, this change filters the supported image format
    list based on the formats that the local Imagick installation supports.

--- src/App/src/Handler/UploadImageFormHandler.php
+++ src/App/src/Handler/UploadImageFormHandler.php
@@ -4,15 +4,27 @@ declare(strict_types=1);
 
 namespace App\Handler;
 
+use Imagick;
 use Laminas\Diactoros\Response\HtmlResponse;
 use Mezzio\Template\TemplateRendererInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
+use function array_intersect;
+
 class UploadImageFormHandler implements RequestHandlerInterface
 {
     private TemplateRendererInterface $renderer;
+
+    public const SUPPORTED_FORMATS = [
+        'AVIF',
+        'GIF',
+        'JPEG',
+        'PNG',
+        'SVG',
+        'WEBP',
+    ];
 
     public function __construct(TemplateRendererInterface $renderer)
     {
@@ -23,7 +35,12 @@ class UploadImageFormHandler implements RequestHandlerInterface
     {
         return new HtmlResponse($this->renderer->render(
             'app::upload-image-form',
-            []
+            [
+                'formats' => array_intersect(
+                    self::SUPPORTED_FORMATS,
+                    Imagick::queryFormats()
+                ),
+            ]
         ));
     }
 }
